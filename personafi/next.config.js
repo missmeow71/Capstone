31eefcc15b48be5caf69f9866ff5c02689b6e87c
missmeow71 <sensuousdesigner@gmail.com>
2019-05-03 22:58:51 -0600

    converted to es6 module

--- personafi/next.config.js
+++ personafi/next.config.js
@@ -1,9 +1,6 @@
-module.exports = {
-  webpack: config => {
-    config.node = {
-      fs: 'empty'
-    }
-
-    return config
-  }
+export function webpack(config) {
+  config.node = {
+    fs: 'empty'
+  };
+  return config;
 }
