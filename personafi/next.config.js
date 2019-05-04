c1fd5e757fa1472a3e7ab88d02449ca1843a8a14
missmeow71 <sensuousdesigner@gmail.com>
2019-05-03 22:23:55 -0600

    changed next.config.js

--- personafi/next.config.js
+++ personafi/next.config.js
@@ -1,9 +1,26 @@
-module.exports = {
-  webpack: config => {
-    config.node = {
-      fs: 'empty'
-    }
+const {
+  PHASE_PRODUCTION_SERVER
+} =
+process.env.NODE_ENV === 'development' ?
+  {} :
+  !process.env.NOW_REGION ?
+  require('next/constants') :
+  require('next-server/constants');
 
-    return config
+module.exports = (phase, {
+  defaultConfig
+}) => {
+  if (phase === PHASE_PRODUCTION_SERVER) {
+    // Skip development logic on production
+    return {};
   }
+  // Development logic
+  const withTypescript = require('@zeit/next-typescript');
+  const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
+  return withTypescript({
+    webpack(config, options) {
+      if (options.isServer) config.plugins.push(new ForkTsCheckerWebpackPlugin());
+      return config;
+    }
+  });
 }
