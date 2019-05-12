4852b76ecc9fc8fc34fcaa6d696b3b7225506a58
missmeow71 <sensuousdesigner@gmail.com>
2019-05-11 20:02:53 -0600

    added to next config

--- personafi/next.config.js
+++ personafi/next.config.js
@@ -1,3 +1,8 @@
+const http = require('http')
+const page = require('./.next/serverless/about.js')
+const server = new http.Server((req, res) => page.render(req, res))
+server.listen(3000, () => console.log('Listening on http://localhost:3000'))
+
 module.exports = 
 {
   target: "serverless",
