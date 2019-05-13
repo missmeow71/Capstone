c0222ae203e16d564c883ad54d7350380e1339a7
missmeow71 <sensuousdesigner@gmail.com>
2019-05-13 10:59:43 -0600

    removed serverless from next config

--- personafi/next.config.js
+++ personafi/next.config.js
@@ -1,11 +1,5 @@
-const http = require('http')
-const page = require('./.next/serverless/about.js')
-const server = new http.Server((req, res) => page.render(req, res))
-server.listen(3000, () => console.log('Listening on http://localhost:3000'))
-
 module.exports = 
 {
-  target: "serverless",
   webpack: config => {
     config.node = {
       fs: 'empty'
